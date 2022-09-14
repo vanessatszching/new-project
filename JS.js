636c41fa34a2a72621ce9c10b911c152e83e5200
vanessatszching <75984573+vanessatszching@users.noreply.github.com>
2022-09-14 21:50:52 +0800

    updateIcon

--- JS.js
+++ JS.js
@@ -58,6 +58,7 @@ function displayWeatherCondition(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
+  document.querySelector("#emojis").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function search(city) {
