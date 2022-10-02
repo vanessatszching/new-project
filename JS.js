8d1902f8f70765fe90dcffa55d1f0f360046afaa
vanessatszching <75984573+vanessatszching@users.noreply.github.com>
2022-10-02 22:08:11 +0800

    homework

--- JS.js
+++ JS.js
@@ -49,8 +49,7 @@ function displayForecast(response) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        `
-      <div class="col-2">
+        `<div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
